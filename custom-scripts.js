6e5796b78e99b73a81648343e54763876a080387
John Knox <knoxboy@gmail.com>
2016-03-12 00:22:13 +0000

    Update custom-scripts.js

--- custom-scripts.js
+++ custom-scripts.js
@@ -5,12 +5,3 @@ if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')
 target.length?target:$('[name='+this.hash.slice(1)+']');if(target.length)
 {$('html,body').animate({scrollTop:target.offset().top},1000);return false;
 }}});});
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-38419707-2', 'jknoxville.com');
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
